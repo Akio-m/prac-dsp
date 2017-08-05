d59deff0ca8bbfc27e0d573f4ae5c66a16b676d2
akio <k168577@ie.u-ryukyu.ac.jp>
2017-08-05 21:15:18 +0900

    fix wav_write.cpp

--- basic-operation/wav_write.cpp
+++ basic-operation/wav_write.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <string>
+#include <cmath>
+#include <cassert>
 #include <math.h>
+
 
 typedef struct {
     char riffID[4];
@@ -22,64 +23,72 @@ typedef struct {
 
 using namespace std;
 
-int main( int argc, char *argv[]) {
+int main( int argc, char *argv[] ) { 
 
     wavHeader header;
 
-    if( argc != 2 ){
-        cout << "Usage: ./a.out output_file.wav"<< endl;
-        exit(-1);
-    }
-    ofstream outputFiles( argv[1] );
+    ofstream of;
+    of.open( "output.wav", ios::out | ios::binary | ios::trunc ); //
+    assert( of );
 
-    memcpy(header.riffID, "RIFF", 4);
-    memcpy(header.waveID, "WAVE", 4);
-    memcpy(header.fmtID, "fmt ", 4);
-    memcpy(header.dataID, "data", 4);
-
-    double f = 1000.0; // sin wave frequency
-    header.dataSize = header.channels * ( 16000 + 0 ) * 2; // 1 sample is 2 bytes
-    header.fileSize = 36 + header.dataSize;
-    header.bytePerSec = header.sampleRate * header.channels * 2;
-    header.blockSize = header.channels * 2; // byte / sample * channels 
-
-    outputFiles << header.riffID;
-    outputFiles << header.fileSize;
-    outputFiles << header.waveID;
-    outputFiles << header.fmtID;
-    outputFiles << header.formatSize;
-    outputFiles << header.formatID; 
-    outputFiles << header.channels; 
-    outputFiles << header.sampleRate;
-    outputFiles << header.bytePerSec;
-    outputFiles << header.blockSize;
-    outputFiles << header.quantizationBitSize;
-    outputFiles << header.dataID;
-    outputFiles << header.dataSize;
+    of.seekp( 0, ios::beg ); //point at file top
     
-    cout << "output data wav is " << argv[1] << endl;
-    cout << "Channel = " << header.channels;
-    cout << "\nSample rate = " << header.sampleRate;
-    cout << "\nSample number = " << header.dataSize / header.channels / 2 << endl;
+    of.write( "RIFF" , 4 );
+    int riff_p = of.tellp();
+    of.seekp( sizeof(char)*4, ios::cur );
+    of.write( "WAVE" , 4 );
+    of.write( "fmt " , 4 );
+    int buf = header.formatSize;
+    of.write( (char*)&buf, 4 );
+    short buf2 = header.formatID;
+    of.write( (char*)&buf2, 2 );
+    buf2 = header.channels;
+    of.write( (char*)&buf2, 2 );
+    buf = header.sampleRate;
+    of.write( (char*)&buf, 4 );
+    header.bytePerSec = 88200;
+    buf = header.bytePerSec;
+    of.write( (char*)&buf, 4 );
+    header.blockSize = 2;
+    buf2 = header.blockSize;
+    of.write( (char*)&buf2, 2 );
+    buf2 = header.quantizationBitSize;
+    of.write( (char*)&buf2, 2 );
+    cout << "data" << endl;
+    of.write( "data" , 4 );
+    int data_p = of.tellp();
+    of.seekp( sizeof(char)*4, ios::cur );
 
-    int t = 0;
-    long int t_out = 0;
-    double s[16000 + 1] = { 0 };
-    double y[16000 + 1] = { 0 };
-    short output = 0;
+    cout << "initialized" << endl;
 
-    while( t_out < 16000 ) {
-        s[t] = 0.5 * sin( 2.0 * M_PI * f / header.sampleRate * t_out );
-        y[t] = s[t];
-        output = y[t] * 23767; // output force to decimal
-        outputFiles << output;
-        if (header.channels == 2) {
-            outputFiles << output; // output for Right channel 
+    static const double f1 = 440.0;
+    static const double f2 = 880.0;
+    static const int t = 5;
+
+    double time = 0.0, fn = 0.0;
+    short out = 0;
+
+    while( (int)time < t ){
+        for( int i=0; i < 16000; i++ ){
+            fn = f1;
+            out = (short)( ( sin( (double)2 * M_PI * fn * (double)i / (double)16000 ) ) * (double)0x7FFF );
+            of.write( (char*)&out, sizeof(short) );
         }
-        t = (t + 1) % 16000;
-        t_out++;
+        cout << "t  = " << time << endl;
+        cout << "fn = " << fn << endl;
+        cout << "----" << endl;
+        time = time + 1.0;
     }
-    
-    outputFiles.close();
+
+    int last_p = of.tellp();
+    of.seekp( riff_p, ios::beg );
+    buf = last_p - 8;
+    of.write( (char*)&buf, 4 );
+    of.seekp( data_p, ios::beg );
+    buf = last_p - data_p - 4;
+    of.write( (char*)&buf, 4 );
+
+    of.close();
+
     return 0;
 }
